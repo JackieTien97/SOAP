e11b14bd74fe586c9aac92f7828c38df6205e8b1
Slow_Time <1395881075@qq.com>
2017-12-28 16:00:28 +0800

    修改接口

--- src/cn/edu/nju/serviceImpl/StudentScoreServiceImpl.java
+++ src/cn/edu/nju/serviceImpl/StudentScoreServiceImpl.java
@@ -1,6 +1,11 @@
 package cn.edu.nju.serviceImpl;
 
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletOutputStream;
 import javax.xml.soap.MessageFactory;
+import javax.xml.soap.SOAPBody;
 import javax.xml.soap.SOAPEnvelope;
 import javax.xml.soap.SOAPException;
 import javax.xml.soap.SOAPMessage;
@@ -11,14 +16,17 @@ import cn.edu.nju.service.StudentScoreService;
 public class StudentScoreServiceImpl implements StudentScoreService {
 
 	@Override
-	public SOAPMessage getStudentScoreById(String id) {
+	public SOAPMessage getStudentScoreById(String id, ServletOutputStream out) {
 		try {
 			SOAPMessage soapMessage = MessageFactory.newInstance().createMessage();
 			SOAPPart soapPart = soapMessage.getSOAPPart();
 			SOAPEnvelope envelope = soapPart.getEnvelope();
 			envelope.setPrefix("env");
+			SOAPBody body = envelope.getBody();
+			soapMessage.writeTo(out);
 		} catch (SOAPException e) {
-			
+			e.printStackTrace();
+		} catch (IOException e) {
 			e.printStackTrace();
 		}
 		return null;
